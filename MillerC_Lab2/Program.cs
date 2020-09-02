deb6cef8720956ec343c421a84694df429e3ac3b
Cari Miller <jumpinlikeitsgotflees@gmail.com>
2020-09-02 18:14:36 -0500

    Add project files.

---
+++ MillerC_Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MillerC_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
